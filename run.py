1d946fe81b275d20e224e141ba7501c070035cef
Zaccary Alperstein <zalperstein@prostatecentre.com>
2018-03-29 11:55:21 -0700

    dfsa

--- run.py
+++ run.py
@@ -17,7 +17,9 @@ def experiment(exp_name):
 	from collections import Counter
 	from random import random
 	from nltk import word_tokenize
-	import train_vaeLM as *
+
+
+    from train_vaeLM import train, prep_perm_matrix, permute_encoder_output
 	from pre import read
 
 	onehot_words,word_pos,sentence_lens_nchars,sentence_lens_nwords,vocabulary_size,max_char_len=read(file='train.h5', train=True)
@@ -39,12 +41,13 @@ def experiment(exp_name):
 	index2token = {index:token for index,token in enumerate(vocabulary)}
 
 
-	train_dict={'batch_size':52,'hidden_size':hidden_size,'decoder_dim':decoder_dim,
-				'max_char_len'=max_char_len,'onehot_words'=onehot_words,'word_pos'=word_pos,'sentence_lens_nchars'=sentence_lens_nchars,
-		'vocabulary_size'=vocabulary_size,'sentence_lens_nwords'=sentence_lens_nwords,
-		'onehot_words' = onehot_words_val, 'word_pos' = word_pos_val, 'sentence_lens_nchars' = sentence_lens_nchars_val,
-			 'sentence_lens_nwords_val' = sentence_lens_nwords_val}
-	network_dict = {'max_char_len': max_char_len, 'batch_size': batch_size,'hidden_size': hidden_size}
+    train_dict = {'batch_size': 52, 'hidden_size': hidden_size, 'decoder_dim': decoder_dim, 'max_char_len': max_char_len,
+                  'onehot_words': onehot_words, 'word_pos': word_pos, 'sentence_lens_nchars': sentence_lens_nchars,
+                  'vocabulary_size': vocabulary_size, 'sentence_lens_nwords': sentence_lens_nwords,
+                  'onehot_words': onehot_words_val, 'word_pos': word_pos_val,
+                  'sentence_lens_nchars': sentence_lens_nchars_val, 'sentence_lens_nwords_val': sentence_lens_nwords_val}
+
+    network_dict = {'max_char_len': max_char_len, 'batch_size': batch_size,'hidden_size': hidden_size}
 
 
 	log("start")
