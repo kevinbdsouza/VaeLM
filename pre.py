877def43b836f2dfcf258ac35921c53d7c0ba76c
Zaccary Alperstein <zalperstein@prostatecentre.com>
2018-03-29 11:57:37 -0700

    fff

--- pre.py
+++ pre.py
@@ -6,55 +6,54 @@ from sys import argv
 
 def write(file,list_inp):
     onehot_words, word_pos, sentence_lens_nchars, sentence_lens_nwords, vocabulary_size, index2token,max_char_len = list_inp
-	with h5py.File(file, "w") as f:
-		g_train = f.create_group('train_group')
+    with h5py.File(file, "w") as f:
+        g_train = f.create_group('train_group')
+        g_train['onehot_words'] = onehot_words
+        g_train['word_pos'] = word_pos
+        g_train['sentence_lens_nchars'] = sentence_lens_nchars
+        g_train['sentence_lens_nwords'] = sentence_lens_nwords
+        g_train['vocabulary_size'] = vocabulary_size
+        #g_train['index2token'] = index2token
+        g_train['max_char_len'] = max_char_len
 
-		g_train['onehot_words'] = onehot_words
-		g_train['word_pos'] = word_pos
-		g_train['sentence_lens_nchars'] = sentence_lens_nchars
-		g_train['sentence_lens_nwords'] = sentence_lens_nwords
-		g_train['vocabulary_size'] = vocabulary_size
-		#g_train['index2token'] = index2token
-		g_train['max_char_len'] = max_char_len
-	
-		g_val = f.create_group('val_group')
-		g_val['onehot_words'] = onehot_words_val
-		g_val['word_pos'] = word_pos_val
-		g_val['sentence_lens_nchars'] = sentence_lens_nchars_val
-		g_val['sentence_lens_nwords'] = sentence_lens_nwords_val
-		g_val['vocabulary_size'] = vocabulary_size_val
-		#g_val['index2token'] = index2token_val
-		g_val['max_char_len'] = max_char_len
+        g_val = f.create_group('val_group')
+        g_val['onehot_words'] = onehot_words_val
+        g_val['word_pos'] = word_pos_val
+        g_val['sentence_lens_nchars'] = sentence_lens_nchars_val
+        g_val['sentence_lens_nwords'] = sentence_lens_nwords_val
+        g_val['vocabulary_size'] = vocabulary_size_val
+        #g_val['index2token'] = index2token_val
+        g_val['max_char_len'] = max_char_len
 
 
 
 def read(file,train=True):
 
-	list_inp=[]
-	with h5py.File(file, "r") as f:
-		if train:
-		    g_train = f.get('train_group')
+    list_inp=[]
+    with h5py.File(file, "r") as f:
+        if train:
+            g_train = f.get('train_group')
 
-		    list_inp.append(g_train['onehot_words'].value)
-		    list_inp.append(g_train['word_pos'].value)
-		    list_inp.append(g_train['sentence_lens_nchars'].value)
-		    list_inp.append(g_train['sentence_lens_nwords'].value)
-		    list_inp.append(g_train['vocabulary_size'].value)
-		    #list_inp.append(g_train['index2token'].value)
-		    list_inp.append(g_train['max_char_len'].value)
+            list_inp.append(g_train['onehot_words'].value)
+            list_inp.append(g_train['word_pos'].value)
+            list_inp.append(g_train['sentence_lens_nchars'].value)
+            list_inp.append(g_train['sentence_lens_nwords'].value)
+            list_inp.append(g_train['vocabulary_size'].value)
+            #list_inp.append(g_train['index2token'].value)
+            list_inp.append(g_train['max_char_len'].value)
 
-		else:
-		    g_val = f.get('val_group')
+        else:
+            g_val = f.get('val_group')
 
-		    list_inp.append(g_val['onehot_words'].value)
-		    list_inp.append(g_val['word_pos'].value)
-		    list_inp.append(g_val['sentence_lens_nchars'].value)
-		    list_inp.append(g_val['sentence_lens_nwords'].value)
-		    list_inp.append(g_val['vocabulary_size'].value)
-		    #list_inp.append(g_val['index2token'].value)
-		    list_inp.append(g_val['max_char_len'].value)
+            list_inp.append(g_val['onehot_words'].value)
+            list_inp.append(g_val['word_pos'].value)
+            list_inp.append(g_val['sentence_lens_nchars'].value)
+            list_inp.append(g_val['sentence_lens_nwords'].value)
+            list_inp.append(g_val['vocabulary_size'].value)
+            #list_inp.append(g_val['index2token'].value)
+            list_inp.append(g_val['max_char_len'].value)
 
-	#onehot_words,word_pos,sentence_lens_nchars,sentence_lens_nwords,vocabulary_size,index2token,max_char_len = list_inp
+    #onehot_words,word_pos,sentence_lens_nchars,sentence_lens_nwords,vocabulary_size,index2token,max_char_len = list_inp
 
 	return list_inp
 
