d586df18298b13e8ffa703f1ad2fb4f0efd86807
Kevin Dsouza <kevindsouza@Kevins-MacBook-Air.local>
2018-03-31 17:28:43 -0700

    reduced max_char

--- encoder.py
+++ encoder.py
@@ -18,10 +18,14 @@ def preprocess(mode):
 	train_sentences = [x for x in train_sentences if x] 
 	val_sentences = [x for x in val_sentences if x] 
 	test_sentences = [x for x in test_sentences if x]
-	max_char_len = 494
+	max_char_len = 371
 
 	if mode == "train":
 	    sentences = train_sentences
+	    pop_list = [4607,38450,24213,27130,28833,39006,38446,20728,2066,11982,2298,18158,4820,29089,24112,35834,
+                    8573,30944,5791,12130,10752,30857,34030,458,35900,3219,7860,10241]
+        for pop in pop_list:
+            sentences.pop(pop)
 	    #max_char_len = 494
 	elif mode == "val":
 	    sentences = val_sentences
