931ffd0be746acb2a881931feb292c2f8c58baf7
zaccary alperstein <zalperst@gmail.com>
2018-04-10 15:07:20 -0700

    train

--- train_vaeLM.py
+++ train_vaeLM.py
@@ -153,7 +153,7 @@ def train(log_dir, n_epochs, network_dict, index2token, **kwargs):
     for batch,batch_len in enumerate(sentence_lens_nwords):
         mat=np.ones(shape=[batch_size,max_char_len],dtype=np.float32)
         for ex, len in enumerate(batch_len):
-            onehot_words[batch, ex, :,27] = onehot_words[batch, ex, :,27]*np.true_divide(2,len)
+            onehot_words[batch, ex, :,27] = onehot_words[batch, ex, :,27]*np.true_divide(len,2)
 
             mat[ex,:]=np.sum(onehot_words[batch,ex,:,:],-1)
 
